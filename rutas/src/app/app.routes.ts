3ab9591425d7845d7c0227d5107539808020c2d4
joselimaico <joselimaicotele@gmail.com>
2018-06-08 17:34:19 -0500

    rutas hijos :+1:

--- rutas/src/app/app.routes.ts
+++ rutas/src/app/app.routes.ts
@@ -2,11 +2,28 @@ import {Routes} from "@angular/router";
 import {RutaInicioComponent} from "./ruta-inicio/ruta-inicio.component";
 import {RutaNoEncontradaComponent} from "./ruta-no-encontrada/ruta-no-encontrada.component";
 import {RutaFAQComponent} from "./ruta-faq/ruta-faq.component";
+import {RutaIniUsuarioComponent} from "./ruta-ini-usuario/ruta-ini-usuario.component";
+import {RutaIniPermisosComponent} from "./ruta-ini-permisos/ruta-ini-permisos.component";
 
 export const RUTAS_APP: Routes = [
   {
     path: 'inicio/hola/como/estas',
-    component: RutaInicioComponent
+    component: RutaInicioComponent,
+    children:[
+      {
+        path:'usuario',
+        component:RutaIniUsuarioComponent
+      },
+      {
+        path:'permisos',
+        component:RutaIniPermisosComponent
+      },
+      {
+        path: '',
+        redirectTo: 'usuario',
+        pathMatch: 'full'
+      },
+    ]
   },
   {
     path: 'faq',
